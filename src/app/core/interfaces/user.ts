c9383c1e5add958d2f325e9ef6c39f08384817ef
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-02 19:59:17 -0300

    Created user interface

---
+++ src/app/core/interfaces/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+  username: string,
+  email: string,
+  password: string,
+}
+
+export interface LoginData {
+  email: string,
+  password: string
+}
+
+export interface RegisterData {
+  username: string,
+  email: string,
+  password: string
+}
