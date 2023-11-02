3b2fc670754a1da8c07b02c7a12972361b4b54c5
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-02 20:17:06 -0300

    Fixed routing module and added missing paths

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    path: "",
+    component: PublicComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+      },
+      {
+        path: 'register',
+        loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+      }
+
+    ]
   }
 ];
 
