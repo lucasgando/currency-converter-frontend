5b3819bd872d4538aad8dd29eec8c0da663b430e
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-02 18:55:02 -0300

    Added guards routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { adminGuard } from './core/guards/admin.guard';
+import { userGuard } from './core/guards/user.guard';
+import { publicGuard } from './core/guards/public.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    canActivateChild: [adminGuard],
+    children: [
+      
+    ],
+  },
+  {
+    canActivateChild: [userGuard],
+    children: [
+      
+    ],
+  },
+  {
+    canActivateChild: [publicGuard],
+    children: [
+      
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
